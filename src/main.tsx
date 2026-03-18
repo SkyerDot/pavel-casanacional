02b20943fbfbfe8d4c7067037949a07c0ba12c15
Júlio Almeida <xeon.skyer@gmail.com>
2026-03-18 00:46:50 -0300

    Fixes para Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import { useEffect } from "react";
-
-function RootWithTitle() {
-	useEffect(() => {
-		document.title = "Casa Nacional - Paulo Coelho";
-	}, []);
-	return <App />;
-}
-
-createRoot(document.getElementById("root")!).render(<RootWithTitle />);
+createRoot(document.getElementById("root")!).render(<App />);
